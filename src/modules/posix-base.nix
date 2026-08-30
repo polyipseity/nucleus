11b3462d424491413b8023da39f522e3622c4c22
William So <polyipseity@gmail.com>
2026-08-31 07:34:45 +0800

    fix(modules): preserve Nix context in POSIX launchd daemon env vars

--- src/modules/posix-base.nix
+++ src/modules/posix-base.nix
@@ -141,7 +141,7 @@ in
             ];
             EnvironmentVariables = {
               NUCLEUS_GC_EXPIRY = config.modules.gc.expiry;
-              NUCLEUS_REPO_ROOT = toString repoRoot;
+              NUCLEUS_REPO_ROOT = "${repoRoot}";
             };
             RunAtLoad = false;
             StartCalendarInterval = [
@@ -160,7 +160,7 @@ in
             EnvironmentVariables = {
               NUCLEUS_GC_EXPIRY = config.modules.gc.expiry;
               NUCLEUS_GC_GENERATIONS_KEEP = toString config.modules.gc.generationsKeep;
-              NUCLEUS_REPO_ROOT = toString repoRoot;
+              NUCLEUS_REPO_ROOT = "${repoRoot}";
               NUCLEUS_USERNAME = username;
             };
             RunAtLoad = false;
