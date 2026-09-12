9cd018d25a72c4d458c1d294c6a9bb9f8772e430
William So <polyipseity@gmail.com>
2026-09-12 20:35:06 +0800

    style(rimsort): fix printf redirect formatting

--- src/scripts/configs/merge-rimsort-json.sh
+++ src/scripts/configs/merge-rimsort-json.sh
@@ -65,6 +65,6 @@ print(Path(os.path.expanduser(path)).parent.parent / 'appworkshop_294100.acf')
 " "$_rimsort_settings_path")"
   if [ -n "$_acf_file" ] && [ ! -f "$_acf_file" ]; then
     mkdir -p "$(dirname "$_acf_file")"
-    printf '"AppWorkshop"\n{\n\t"appid"\t\t\t"294100"\n\t"SizeOnDisk"\t\t"0"\n\t"needsUpdate"\t\t"0"\n\t"TimeLastUpdated"\t"0"\n\t"WorkshopItemsInstalled"\n\t{\n\t}\n}\n' > "$_acf_file"
+    printf '"AppWorkshop"\n{\n\t"appid"\t\t\t"294100"\n\t"SizeOnDisk"\t\t"0"\n\t"needsUpdate"\t\t"0"\n\t"TimeLastUpdated"\t"0"\n\t"WorkshopItemsInstalled"\n\t{\n\t}\n}\n' >"$_acf_file"
   fi
 fi
